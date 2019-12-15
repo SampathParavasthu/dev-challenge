9e1186611e1c047d77933b2e6dd5323bf2408115
Sampath Paravasthu <write2sampath@gmail.com>
2019-12-15 22:29:32 +0530

    137 done

--- daily-challenge-137.js
+++ daily-challenge-137.js
@@ -1,20 +1,21 @@
 let dailyChallenge = (books, codes) => {
   let bookCodes = codes.reduce((result, code) => {
-    books.forEach((book) => {
+    books.forEach(book => {
       if (book[0] === code) {
-        result[code] = (result[code] || 0) + (+book.split(' ')[1]);
+        result[code] = (result[code] || 0) + +book.split(' ')[1];
       } else {
         result[code] = (result[code] || 0) + 0;
       }
     });
     return result;
   }, {});
-  if (Object.keys(bookCodes).length === 0) {
-    return '';
-  } else {
-    return bookCodes;
-  }
-}
+
+  let finalCodes = Object.keys(bookCodes)
+    .map(key => `(${key} : ${bookCodes[key]})`)
+    .join(' - ');
+
+  return finalCodes || '';
+};
 
 const books = ['ABART 20', 'CDXEF 50', 'BKWRK 25', 'BTSQZ 89', 'DRTYM 60'];
 const codes = ['A', 'B', 'C', 'W'];
